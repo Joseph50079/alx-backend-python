3121ac70d9e27e19cc505f4a2ddb8d0e31498c29
Joseph50079 <josephogbah2019@gmail.com>
2024-10-30 18:48:35 +0100

    documentation fixed

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -8,4 +8,6 @@ TYPE = typing.List[typing.Tuple[typing.Sequence, int]]
 
 
 def element_length(lst: typing.Iterable[typing.Sequence]) -> TYPE:
+    """annotate typing Sequence"""
+
     return [(i, len(i)) for i in lst]
