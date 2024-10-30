df90dad5409c3e473685892c777ae13cdfdf673b
Joseph50079 <josephogbah2019@gmail.com>
2024-10-30 18:46:03 +0100

    done now

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""annotate this module"""
+
+import  typing
+
+TYPE = typing.List[typing.Tuple[typing.Sequence, int]]
+
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> TYPE:
+        return [(i, len(i)) for i in lst]
