0315a1c701385b34dd17d1c17221238020e9bcc4
Joseph50079 <josephogbah2019@gmail.com>
2024-10-30 17:18:52 +0100

    sum list type

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Module sum list"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """returns sum of the list as float"""
+
+    i = sum(input_list)
+    return float(i)
